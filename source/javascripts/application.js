24e119528aac24e332eb6c503f142f2559872616
Michał Martyniak <michal.martyniak@linux.pl>
2017-12-14 20:37:08 +0100

    Refactor 'Expand' buttons to use jQuery data
    Change recent projects page proportions (wider major section)
    Fix unnecessary dark bar at the top of the page
    Add third recent project
    Refactor yml and erb for major projects (allow for expand description button)

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,15 +1,25 @@
-// Show more functionality
-$(".show-more a").on("click", function () {
+$(function() {
+    $("div.content").each(function() {
+        classes = this.classList;
+        if (["showContent"].indexOf(classes) >= 0) {
+            $(this).data("hidden", false);
+        } else if (["hideContent"].indexOf(classes) >= 0) {
+            $(this).data("hidden", true);
+        }
+    });
+});
+$(".show-more a").on("click", function() {
     var $anchor = $(this);
     var $content = $anchor.parent().prev('div.content');
     var linkText = $anchor.text();
-
-    if (linkText === 'Show more') {
-        linkText = 'Show less';
-        $content.switchClass('hideContent', 'showContent', 400);
+    if ($anchor.data("hidden") === true) {
+        linkText = "Collapse ▲";
+        $content.switchClass("hideContent", "showContent", 400);
+        $anchor.data("hidden", false);
     } else {
-        linkText = 'Show more';
-        $content.switchClass('showContent', 'hideContent', 400);
-    };
-    $anchor.context.text = linkText ;
+        linkText = "Expand ▼";
+        $content.switchClass("showContent", "hideContent", 400);
+        $anchor.data("hidden", true);
+    }
+    $anchor.context.text = linkText;
 });
